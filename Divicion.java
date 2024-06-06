a52da27bcd712ec3b3dfd065fc761eec4dab60d4
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-06-05 21:50:55 -0600

    Create Divicion.java

---
+++ Divicion.java
@@ -0,0 +1,10 @@
+public class Divicion {
+    public static void main(String[] args) {
+        double num1 = 2;
+        double num2 = 38;
+        double res = 0;
+        res = num1/num2;
+        System.out.println("El resultado de dividir " +num2+ "/" + num1 +" = "+res);
+    }
+    
+}
